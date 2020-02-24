5982ce96f6e05327ddb1b3192ba8a518eccd3c75
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-02-23 20:02:41 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 USERNAME = 'admin'
 PASSWORD = 'password123'
 
-UPLOAD_FOLDER = './app/static/uploads'
+UPLOAD_FOLDER = 'app/static/uploads'
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
 SECRET_KEY = 'Sup3r$3cretkey'
